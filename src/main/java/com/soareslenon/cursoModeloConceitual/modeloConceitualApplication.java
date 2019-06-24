e28e68cc3e9edac4fa5bbf0623fb3a77ed69bf54
Lenon Soares <lenonpolly@gmail.com>
2019-06-24 16:03:38 -0300

    Projeto Criado

---
+++ src/main/java/com/soareslenon/cursoModeloConceitual/modeloConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.soareslenon.cursoModeloConceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class modeloConceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(modeloConceitualApplication.class, args);
+	}
+
+}
